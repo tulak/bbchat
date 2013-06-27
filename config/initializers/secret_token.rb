780e346f7db9e6f3da0a03d79a810f7c400676c6
Filip Zachar <tulak@netspace.sk>
2013-06-27 12:30:51 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bbchat::Application.config.secret_key_base = 'c3a86d93198e99624d32c745097daba47866f91a429a6907426bef34a45c42c5dff81b19087e6f1d04f248b257fd2d839a05fa441789d2a6cab38a040745523c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bbchat::Application.config.session_store :cookie_store, key: '_bbchat_session'
